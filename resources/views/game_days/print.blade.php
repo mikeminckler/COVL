2f4185df2c678d610e24f07e9d32d345d7c30001
Mike Minckler <mikeminckler@gmail.com>
2015-05-20 12:31:25 -0700

    Updated printing of game days

--- resources/views/game_days/print.blade.php
+++ resources/views/game_days/print.blade.php
@@ -14,7 +14,7 @@
 
 		<h2>{{ $league->league_name }}</h2>
 
-		@include ('game_days.rounds', ['hide_teams' => true, 'best_of' => 3])
+		@include ('game_days.rounds', ['hide_teams' => null, 'best_of' => 3])
 
 		<div style="clear: both; page-break-after: always;"></div>
 
